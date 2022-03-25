3a6df0d6daa11965c357ebd14e3b7ccc869b675e
Oroshdy <92540795+Oroshdy@users.noreply.github.com>
2022-03-25 19:03:22 +0200

    Add files via upload

---
+++ Question 1.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+x = input( "First Name: ")
+
+       
+
+
+# In[ ]:
+
+
+
+y = input ("Last Name :")     
+
+
+# In[ ]:
+
+
+
+print(y,"",x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# 
